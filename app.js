3452fde7f962ab625a03e9863500c5d4d9d840b0
Guido <Guido@hotmail.com.ar>
2019-02-20 12:07:09 -0300

    Termino funcionalidad de geolocalizacion

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let posicion = ubicacion => {
-    console.log(ubicacion.coords.latitude)
-    console.log(ubicacion.coords.longitude)
+    let lat = ubicacion.coords.latitude
+    let lon = ubicacion.coords.longitude
+    let p = document.createElement("p")
+    p.innerText = `Mi ubicacion es ${lat} y ${lon}`
+    document.body.appendChild(p)
 }
 
 navigator.geolocation.getCurrentPosition(posicion)
